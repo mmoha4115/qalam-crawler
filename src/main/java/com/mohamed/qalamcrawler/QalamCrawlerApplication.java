11e4e72e7ea6241d5d2cad0749939e1b21e0653f
mmoha4115 <mmohamed54d0qf@gmail.com>
2018-12-16 18:34:17 -0600

    initial commit

---
+++ src/main/java/com/mohamed/qalamcrawler/QalamCrawlerApplication.java
@@ -0,0 +1,14 @@
+package com.mohamed.qalamcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QalamCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QalamCrawlerApplication.class, args);
+	}
+
+}
+
